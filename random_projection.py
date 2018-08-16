40ec1006cfd5314e52ae6ad93d77d844c3f649c4
Yang Guo <yg93@cornell.edu>
2018-08-16 21:04:15 +0800

    create simulation class

--- random_projection.py
+++ random_projection.py
@@ -1,31 +1,99 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-x = np.random.normal(0, 1, 10000)
-
-print(x.shape)
+import tensorly as tl
 
 
-rp_res = []
-srp_res = []
+class Simulation(object):
+    """docstring for Simulation""" 
+    '''
+    :param X: input matrix of size m x n 
+    :param sim_typ: type of the simulation "len" (length preservation), "col": 
+        (column space)
+    :param k: the reduced dimension
+    :param split: k/split[0] * m/split[1] is the size of the kronecker component 
+    '''
+    def __init__(self, X, sim_typ, k, split = (10, 10), num_runs = 100, seed = 1):
+        self.X = X
+        self.sim_typ = sim_typ  
+        self.k = k
+        self.split = split
+        self.num_runs = num_runs
+        self.seed = seed 
+
+    def run_sim(self):
+        m, n = self.X.shape 
+        if self.sim_typ == "len": 
+            gaussian_err = [] 
+            krao_err = []
+            kron_err = [] 
+            for i in range(self.num_runs):
+                # type of the random matrices: "kron" (Kronecker), "krao" (Khatri-Rao)
+                mat_gauss = np.random.normal(0, 1, (self.k,m))/np.sqrt(self.k)
+                gaussian_err.append(np.linalg.norm(mat_gauss @ self.X)\
+                    /np.linalg.norm(self.X))
+                print(self.split[0])
+                print(int(m/self.split[0]))
+                mat_krao1 = np.random.normal(0, 1, (self.k,int(m/self.split[0]))) 
+                print(mat_krao1.shape)
+                mat_krao2 = np.random.normal(0, 1, (self.k,self.split[0]))
+                print(mat_krao2.shape)
+                mat_krao = tl.tenalg.khatri_rao([mat_krao1.T,mat_krao2.T]).T
+                print(mat_krao.shape)
+                krao_err.append(np.linalg.norm\
+                    (mat_krao @ self.X)\
+                    /np.sqrt(10)/np.linalg.norm(self.X)) 
+                mat_kron1 = np.random.normal(0,1,(self.k,\
+                    (m/self.kintron_split))) 
+                mat_kron2 = np.random.normal(0,1,(int(m/self.k),\
+                 self.kron_split))  
+                mat_kron = np.kron(mat_kron1,mat_kron2)
+                kron_err.append(np.linalg.norm(mat_kron @ self.X)\
+                    /np.sqrt(k)/np.linalg.norm(self.X)) 
+            rel_err = [gaussian_err,krao_err,kron_err]
+        elif self.sim_typ == "col": 
+            gaussian_err = [] 
+            krao_err = []
+            kron_err = [] 
+            for i in range(self.num_runs):
+                mat_gauss = np.random.normal(0, 1, (self.k,m))
+                mat_krao1 = np.random.normal(0, 1, (self.k,m)) 
+                mat_krao2 = np.random.normal(0, 1, (int(m/self.k),m))
+                mat_kron1 = np.random.normal(0,1,(self.k,\
+                    (m/self.kintron_split))) 
+                mat_kron2 = np.random.normal(0,1,\
+                    (int(m/self.k), self.kron_split)) 
+                mat_krao = tl.tenalg.khatri_rao([mat_krao1.T,mat_krao2.T]).T
+                mat_kron = np.kron(mat_kron1,mat_kron2)
+                gaussian_err.append(np.linalg.norm(A.T @ A @ mat_gauss-self.X)\
+                    /np.linalg.norm(self.X)) 
+                krao_err.append(np.linalg.norm(A.T @ A @ mat_krao-self.X)\
+                    /np.linalg.norm(self.X))  
+                kron_err.append(np.linalg.norm(A.T @ A @ mat_kron-self.X)\
+                    /np.linalg.norm(self.X)) 
+            rel_err = [gaussian_err,krao_err,kron_err]
+        return rel_err
+
+if __name__ == '__main__':
+    X1 = np.random.normal(0,1, (10000,1000)) 
+    sim_len = Simulation(X1, "len", 10, (5,10)) 
+    rel_err = sim_len.run_sim() 
+
+    plt.figure()
+    plt.hist(rel_err[gaussian_err])
+    plt.hist(rel_err[krao_err])
+    plt.hist(rel_err[kron_err])
+    plt.show()
+
+    # rank = 10
+    X2 =  np.diag(np.repeat(10,10),np.rep(0,615))  
+    X2 = X2 + np.random.normal(0,1,(625, 625)) 
+    sim_col = Simulation(X2, "len", 25, 25) 
+    rel_err = sim_col.run_sim() 
+
+    plt.figure()
+    plt.hist(rel_err[gaussian_err])
+    plt.hist(rel_err[krao_err])
+    plt.hist(rel_err[kron_err])
+    plt.show()
 
 
-for i in range(100):
-    A = np.random.normal(0, 1, (10,10000))/np.sqrt(10)
-    rp_res.append(np.linalg.norm(np.dot(A,x)))
-    A = np.random.normal(0, 1, (10, 100))
-    B = np.random.normal(0, 1, (10, 100))
-    rm = np.kron(A,B)
-    srp_res.append(np.linalg.norm(np.dot(np.kron(A,B)/10, x)))
-
-print(np.linalg.norm(x))
-print(np.mean(rp_res))
-print(np.mean(srp_res))
-
-
-print(np.std(rp_res))
-print(np.std(srp_res))
-
-plt.hist(rp_res)
-plt.hist(srp_res)
-plt.show()

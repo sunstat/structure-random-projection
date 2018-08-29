f3e3049aac27ebab466e3cdfea3b06acfd9513ef
Yang Guo <yg93@cornell.edu>
2018-08-29 15:25:06 -0500

    add sparse map

--- random_projection.py
+++ random_projection.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorly as tl
 
-def random_matrix_generator(m, n, typ = 'g', std = 1, sparse_factor = 0.1):
+def random_matrix_generator(m, n, typ = 'g', std = 1, sparse_factor = 0.5):
     types = set(['g', 'u', 'sp'])
     assert typ in types, "please aset your type of random variable correctly"
     if typ == 'g':
@@ -22,13 +22,14 @@ class Simulation(object):
     :param k: the reduced dimension
     :param split: k/split[0] * m/split[1] is the size of the kronecker component 
     '''
-    def __init__(self, X, sim_typ, k, split = (10, 10), num_runs = 100, seed = 1):
+    def __init__(self, X, sim_typ, k, split = (10, 10), gen_typ = 'g', num_runs = 100, seed = 1):
         self.X = X
         self.sim_typ = sim_typ  
         self.k = k
         self.split = split
         self.num_runs = num_runs
         self.seed = seed 
+        self.gen_typ = gen_typ
 
     def run_sim(self):
         m, n = self.X.shape 
@@ -38,19 +39,19 @@ class Simulation(object):
             kron_err = [] 
             for i in range(self.num_runs):
                 # type of the random matrices: "kron" (Kronecker), "krao" (Khatri-Rao)
-                mat = random_matrix_generator(self.k,m)/np.sqrt(self.k)
+                mat = random_matrix_generator(self.k,m, self.gen_typ)/np.sqrt(self.k)
                 gaussian_err.append(np.linalg.norm(mat @ self.X)\
                     /np.linalg.norm(self.X))
-                mat_krao1 = random_matrix_generator(self.k,int(m/self.split[1]))
-                mat_krao2 = random_matrix_generator(self.k,self.split[1])
+                mat_krao1 = random_matrix_generator(self.k,int(m/self.split[1]), self.gen_typ)
+                mat_krao2 = random_matrix_generator(self.k,self.split[1], self.gen_typ)
                 mat_krao = tl.tenalg.khatri_rao([mat_krao1.T,mat_krao2.T]).T
                 krao_err.append(np.linalg.norm\
                     (mat_krao @ self.X)\
                     /np.sqrt(self.k)/np.linalg.norm(self.X)) 
                 mat_kron1 = random_matrix_generator(int(self.k/self.split[0]),\
-                    int(m/self.split[1]))
+                    int(m/self.split[1]), self.gen_typ)
                 mat_kron2 = random_matrix_generator(self.split[0],\
-                 self.split[1])
+                 self.split[1], self.gen_typ)
                 mat_kron = np.kron(mat_kron1,mat_kron2)
                 kron_err.append(np.linalg.norm(mat_kron @ self.X)\
                     /np.sqrt(self.k)/np.linalg.norm(self.X)) 
@@ -62,10 +63,10 @@ class Simulation(object):
             for i in range(self.num_runs):
                 print(i)
                 mat = random_matrix_generator(self.k,m)
-                mat_krao1 = random_matrix_generator(self.k,int(m/self.split[1])) 
-                mat_krao2 = random_matrix_generator(self.k,int(self.split[1]))
+                mat_krao1 = random_matrix_generator(self.k,int(m/self.split[1]), self.gen_typ) 
+                mat_krao2 = random_matrix_generator(self.k,int(self.split[1]), self.gen_typ)
                 mat_kron1 = random_matrix_generator(self.k/self.split[0],\
-                    (m/self.split[1]))
+                    (m/self.split[1]), self.gen_typ)
                 mat_kron2 = random_matrix_generator \
                     (self.split[0], self.split[1])
                 mat_krao = tl.tenalg.khatri_rao([mat_krao1.T,mat_krao2.T]).T
@@ -82,7 +83,7 @@ class Simulation(object):
 if __name__ == '__main__':
 
     X1 = np.random.normal(0,1, (10000,1000)) 
-    sim_len = Simulation(X1, "len", 20, (5,10)) 
+    sim_len = Simulation(X1, "len", 20, (5,10), 'sp') 
     rel_err = sim_len.run_sim() 
 
     plt.figure()
@@ -94,6 +95,9 @@ if __name__ == '__main__':
     print("kron: mean" + str(np.mean(rel_err[2]))+"variance:" +str(np.var(rel_err[2])))
     plt.show()
 
+
+'''
+    # Column space
     # rank = 10
     X2 = np.diag(np.hstack((np.repeat(10,10), np.repeat(0,990))))
     X2 = X2 + np.random.normal(0,1,(1000, 1000)) 
@@ -112,5 +116,5 @@ if __name__ == '__main__':
     print("kron: mean: " + str(np.mean(rel_err[2]))+" variance: " \
         +str(np.var(rel_err[2])))
     plt.show()
-
+'''
 
